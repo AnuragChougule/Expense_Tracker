ef4c0350cbf89796e2369941aa75beabc97d85a8
AnuragChougule <131390216+AnuragChougule@users.noreply.github.com>
2025-11-20 13:08:22 +0530

    reverted changes back

--- server.js
+++ server.js
@@ -14,7 +14,11 @@
     app.use(express.static(path.join(__dirname, 'public')));
 
     // Connect to MongoDB
-   mongoose.connect(MONGO_URI);
+  mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
         .then(() => console.log('Connected to MongoDB'))
         .catch((err) => console.error('Error connecting to MongoDB:', err));
