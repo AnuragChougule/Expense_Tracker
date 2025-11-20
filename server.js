2318709be63e04f160a58e811129c15a72834ff0
AnuragChougule <131390216+AnuragChougule@users.noreply.github.com>
2025-11-20 13:03:33 +0530

    New code

--- server.js
+++ server.js
@@ -14,11 +14,8 @@
     app.use(express.static(path.join(__dirname, 'public')));
 
     // Connect to MongoDB
-    mongoose
-        .connect('mongodb://127.0.0.1:27017/expense_tracker', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+   mongoose.connect(MONGO_URI);
+
         .then(() => console.log('Connected to MongoDB'))
         .catch((err) => console.error('Error connecting to MongoDB:', err));
 
